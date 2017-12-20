3b6bb45b168ad51fd71897cda1ed1b9199f0d371
douria <douria.zen@gmail.com>
2017-12-20 09:15:43 +0100

    testé la compilation

--- test_res.py
+++ test_res.py
@@ -4,26 +4,24 @@ Created on Wed Dec 13 12:00:07 2017
 
 @author: douria
 """
+import numpy as np
 
-import random
-
-
-with open("iris_test","r") as fichier:
+with open("iris_test.txt","r") as fichier:
     iris_res=fichier.read()
 
 listIris = iris_res.split("\n")
-iris_res.close()
-
+W=np.array([1, 2, 3, 4, 5])
+#W={2,3,4,8,1,2,6}
 perf=0
-N=length(listIris)
+N=len(listIris)
 
 for i in range (0,N-1) :
     # on calcule le produit sclaire 
     produitScalaire=W[0]
     x=listIris[i].split(" ")
-    for j in range(1,length(W)-1):
-        somme=somme+x[j]*W[j]
-    if((x[0]*somme)>0)
+    for j in range(1,len(W)-1):
+        produitScalaire=produitScalaire+(float(x[j])*float(W[j]))
+    if((float(x[0])*produitScalaire)>0):
         perf=perf+1
 
 #on affiche le pourcentage de bonne classification       
