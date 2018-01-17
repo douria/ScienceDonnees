d1f4417ed46e4c94343097b1e7b34d23c0ee0e18
alice2909 <alice.rivoal@gmail.com>
2018-01-17 10:54:40 +0100

    perceptron et Adaline ok

--- adaline.py
+++ adaline.py
@@ -7,47 +7,52 @@ Created on Wed Jan 10 09:20:03 2018
 
 import random
 import numpy as np
-import Science_donnees as sdd
 
-## Dans un premier temps on récupère les éléments du fichier iris apprentissage
-#with open("iris_apprentissage.txt","r") as fichier:
-#    iris_apprentissage_res=fichier.read()
-#
-#listIris = iris_apprentissage_res.split("\n")
-#
-## On sépare pour chacun des éléments les différentes valeurs de leurs caractéristiques
-#mylist = [(listIris[i].split(" ")) for i in range(0,len(listIris)-1)]
-#
-## Enfin on cast les éléments de la liste pour en faire des nombres float au lieu de string
-#for i in range(len(mylist)):
-#    for j in range(len(mylist[i])):
-#        try:
-#            mylist[i][j] = float(mylist[i][j])
-#        except ValueError,e:
-#            print "error",e,"on line",i, j
-#
-#mylist_norm = mylist
-##fonction pour normaliser
-#def maxi(tab, ind):
-#    m = tab[0][ind]
-#    for i in range(1,len(tab)):
-#        if tab[i][ind]>m:
-#            m=tab[i][ind]
-#    return m
-#    
-##on normalise les valeurs
-#for i in range(1, len(mylist[0])):
-#    maxim = maxi(mylist, i)
-#    for j in range(0, len(mylist)):
-#        mylist_norm[j][i] = mylist[j][i]/maxim
-
+def recuplistfich(name):
+    # Dans un premier temps on récupère les éléments du fichier iris apprentissage
+    with open(name,"r") as fichier:
+        apprentissage_res=fichier.read()
+    
+    list_res = apprentissage_res.split("\n")
+    
+    # On sépare pour chacun des éléments les différentes valeurs de leurs caractéristiques
+    mylist = [(list_res[i].split(" ")) for i in range(0,len(list_res)-1)]
+    
+    # Enfin on cast les éléments de la liste pour en faire des nombres float au lieu de string
+    for i in range(len(mylist)):
+        for j in range(len(mylist[i])):
+            try:
+                mylist[i][j] = float(mylist[i][j])
+            except ValueError,e:
+                print "error",e,"on line",i, j
+    
+    return mylist
 
 #  récupérer les éléments
-listNormRBCancer = sdd.RBCancer
-listNormIris = sdd.iris
-listNormMushroom = sdd.mushroom
-listNormSpambase = sdd.spambase
+iris_list = recuplistfich("iris_apprentissage.txt")
+#bCancer_list = recuplistfich("bCancer_apprentissageN.txt")
+#mushroom_list = recuplistfich("mushroom_apprentissageN.txt")
+#spambase_list = recuplistfich("spambase_apprentissageN.txt")
 
+def norm(mylist):
+    mylist_norm = mylist
+    #fonction pour normaliser
+    def maxi(tab, ind):
+        m = tab[0][ind]
+        for i in range(1,len(tab)):
+            if tab[i][ind]>m:
+                m=tab[i][ind]
+        return m
+        
+    #on normalise les valeurs
+    for i in range(1, len(mylist[0])):
+        maxim = maxi(mylist, i)
+        for j in range(0, len(mylist)):
+            mylist_norm[j][i] = mylist[j][i]/maxim
+    
+    return mylist_norm
+
+iris_list = norm(iris_list)
 
 # Autres initialisations
 #le pas d'appentissage
@@ -55,9 +60,12 @@ mu_tab = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
 #précision
 E = 0.01
 #le nombre max d'itération
-T = 20*len(mylist)
+Tiris = 20*len(iris_list)
+#TbCancer = 20*len(bCancer_list)
+#Tmushroom = 20*len(mushroom_list)
+#Tspambase = 20*len(spambase_list)
 
-resultat_adaline= []
+
 
 # Définition des fonctions permettant de calculer Adaline
 def produitScalaire(w, x):
@@ -85,39 +93,40 @@ def adaline(mu, T, E, liste):
         t = t+1
     return w
 
-# On exécute Adaline pour les différentes valeurs de mu 
-for m in range(0,len(mu_tab)):
-    resultat_adaline.append(adaline(mu_tab[m], T, E, mylist_norm))
+def executeAdaline(listNorm, T, mu_tab, E):
+    resultat_adaline= []
+    # On exécute Adaline pour les différentes valeurs de mu 
+    for m in range(0,len(mu_tab)):
+        resultat_adaline.append(adaline(mu_tab[m], T, E, listNorm))
+    
+    print (resultat_adaline)
+    return resultat_adaline
 
-print (resultat_adaline)
-
-
+print("Iris Apprentissage:\n")
+resultat_adaline_Iris = executeAdaline(iris_list, Tiris, mu_tab, E)
+#print("BCancer Apprentissage:\n")
+#resultat_adaline_BCancer = executeAdaline(bCancer_list, TbCancer, mu_tab, E)
+#print("Mushroom Apprentissage:\n")
+#resultat_adaline_Mushroom = executeAdaline(mushroom_list, Tmushroom, mu_tab, E)
+#print("Spambase Apprentissage:\n")
+#resultat_adaline_Spambase = executeAdaline(spambase_list, Tspambase, mu_tab, E)
 
 # Partie test
-# Dans un premier temps on récupère les éléments du fichier iris test
-with open("iris_test.txt","r") as fichier:
-    iris_test_res=fichier.read()
 
-listIrisTest = iris_test_res.split("\n")
-
-# On sépare pour chacun des éléments les différentes valeurs de leurs caractéristiques
-mylistTest = [(listIrisTest[i].split(" ")) for i in range(0,len(listIrisTest)-1)]
-
-# Enfin on cast les éléments de la liste pour en faire des nombres float au lieu de string
-for i in range(len(mylistTest)):
-    for j in range(len(mylistTest[i])):
-        try:
-            mylistTest[i][j] = float(mylistTest[i][j])
-        except ValueError,e:
-            print "error",e,"on line",i, j
-
+iris_listTest = recuplistfich("iris_test.txt")
+#bCancer_listTest = recuplistfich("bCancer_testN.txt")
+#mushroom_listTest = recuplistfich("mushroom_testN.txt")
+#spambase_listTest = recuplistfich("spambase_testN.txt")
 
 # Autres initialisations
-N=len(mylistTest)
+Niris = len(iris_listTest)
+#NbCancer = len(bCancer_listTest)
+#Nmushroom = len(mushroom_listTest)
+#Nspambase = len(spambase_listTest)
 perf_tab = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
-# Fonction de calcul de la performance sur l'échantillon test
-def testerAdaline(w_res, liste):
+# Fonctions de calcul de la performance sur l'échantillon test
+def testerAdaline(w_res, liste, N):
     perf = 0.0
     for i in range(0, N):
         ps = produitScalaire(w_res, liste[i])
@@ -126,10 +135,24 @@ def testerAdaline(w_res, liste):
     print(perf/float(N))
     return perf/float(N)
 
-#for i in range(0,len(resultat_adaline)):
-#    print(resultat_adaline[i])
-#    r = testerAdaline(resultat_adaline[i], mylistTest)
-#    print(r)
-#    perf_tab[i] = r
+def perfAdaline(w_res, mylistTest, N):    
+    for i in range(0,len(w_res)):
+        print(w_res[i])
+        r = testerAdaline(w_res[i], mylistTest, N)
+        print(r)
+        perf_tab[i] = r
+    return perf_tab
 
-#print (perf_tab)
+perf_Iris = perfAdaline(resultat_adaline_Iris,iris_listTest,Niris)
+#perf_bCancer = perfAdaline(resultat_adaline_BCancer,bCancer_listTest,NbCancer)
+#perf_mushroom = perfAdaline(resultat_adaline_Mushroom,mushroom_listTest,Nmushroom)
+#perf_spambase = perfAdaline(resultat_adaline_Spambase,spambase_listTest,Nspambase)
+
+print ("\n Performances Adaline Iris: \n")
+print (perf_Iris)
+#print ("\n Performances Adaline bCancer: \n")
+#print (perf_bCancer)
+#print ("\n Performances Adaline Mushroom: \n")
+#print (perf_mushroom)
+#print ("\n Performances Adaline Spambase: \n")
+#print (perf_spambase)
--- perceptron.py
+++ perceptron.py
@@ -7,31 +7,26 @@ Created on Wed Dec 20 08:33:07 2017
 import random
 import numpy as np
 
+def recuplistfich(name):
+    # Dans un premier temps on récupère les éléments du fichier iris apprentissage
+    with open(name,"r") as fichier:
+        apprentissage_res=fichier.read()
+    
+    list_res = apprentissage_res.split("\n")
+    
+    # On sépare pour chacun des éléments les différentes valeurs de leurs caractéristiques
+    mylist = [(list_res[i].split(" ")) for i in range(0,len(list_res)-1)]
+    
+    # Enfin on cast les éléments de la liste pour en faire des nombres float au lieu de string
+    for i in range(len(mylist)):
+        for j in range(len(mylist[i])):
+            try:
+                mylist[i][j] = float(mylist[i][j])
+            except ValueError,e:
+                print "error",e,"on line",i, j
+    
+    return mylist
 
-
-
-# Dans un premier temps on récupère les éléments du fichier iris apprentissage
-with open("iris_apprentissage.txt","r") as fichier:
-    iris_apprentissage_res=fichier.read()
-
-listIris = iris_apprentissage_res.split("\n")
-
-# On sépare pour chacun des éléments les différentes valeurs de leurs caractéristiques
-mylist = [(listIris[i].split(" ")) for i in range(0,len(listIris)-1)]
-
-# Enfin on cast les éléments de la liste pour en faire des nombres float au lieu de string
-for i in range(len(mylist)):
-    for j in range(len(mylist[i])):
-        try:
-            mylist[i][j] = float(mylist[i][j])
-        except ValueError,e:
-            print "error",e,"on line",i, j
-
-# Autres initialisations
-mu_tab = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
-T = 7
-
-resultat_perceptron= []
 
 # Définition des fonctions permettant de calculer le Perceptron
 def produitScalaire(w, x):
@@ -52,51 +47,85 @@ def perceptron(mu, T, liste):
                 w[i] = w[i]+ mu*elt[0]*elt[i]
     return w
 
-# On exécute le Perceptron pour les différentes valeurs de mu 
-for m in range(0,len(mu_tab)):
-    resultat_perceptron.append(perceptron(mu_tab[m], T, mylist))
+def executePerceptron(mylist, T, mu_tab):
+    resultat_perceptron= []
+    # On exécute Perceptron pour les différentes valeurs de mu 
+    for m in range(0,len(mu_tab)):
+        resultat_perceptron.append(perceptron(mu_tab[m], T, mylist))
+    #print (resultat_perceptron)
+    
+    return resultat_perceptron
 
-print (resultat_perceptron)
-
-
-
-# Partie test
-# Dans un premier temps on récupère les éléments du fichier iris test
-with open("iris_test.txt","r") as fichier:
-    iris_test_res=fichier.read()
-
-listIrisTest = iris_test_res.split("\n")
-
-# On sépare pour chacun des éléments les différentes valeurs de leurs caractéristiques
-mylistTest = [(listIrisTest[i].split(" ")) for i in range(0,len(listIrisTest)-1)]
-
-# Enfin on cast les éléments de la liste pour en faire des nombres float au lieu de string
-for i in range(len(mylistTest)):
-    for j in range(len(mylistTest[i])):
-        try:
-            mylistTest[i][j] = float(mylistTest[i][j])
-        except ValueError,e:
-            print "error",e,"on line",i, j
-
-
-# Autres initialisations
-N=len(mylistTest)
-perf_tab = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 # Fonction de calcul de la performance sur l'échantillon test
-def testerPerceptron(w_res, liste):
+def testerPerceptron(w_res, liste, N):
     perf = 0.0
     for i in range(0, N):
         ps = produitScalaire(w_res, liste[i])
         if((liste[i][0]*ps)>0):
             perf=perf+1.0
-    print(perf/float(N))
+    #print(perf/float(N))
     return perf/float(N)
 
-for i in range(0,len(resultat_perceptron)):
-    print(resultat_perceptron[i])
-    r = testerPerceptron(resultat_perceptron[i], mylistTest)
-    print(r)
-    perf_tab[i] = r
+def perfPerceptron(resultat_perceptron,mylistTest, N):
+    perf_tab = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    for i in range(0,len(resultat_perceptron)):
+        #print(resultat_perceptron[i])
+        r = testerPerceptron(resultat_perceptron[i], mylistTest, N)
+        #print(r)
+        perf_tab[i] = r
+    
+    #print (perf_tab)
+    return perf_tab
 
-print (perf_tab)
+
+# Partie Apprentissage
+#  récupérer les éléments
+iris_list = recuplistfich("iris_apprentissage.txt")
+#bCancer_list = recuplistfich("bCancer_apprentissage.txt")
+#mushroom_list = recuplistfich("mushroom_apprentissage.txt")
+#spambase_list = recuplistfich("spambase_apprentissage.txt")
+
+# Autres initialisations
+mu_tab = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+T = 7
+
+print("Iris Apprentissage:\n")
+resultat_perceptron_Iris = executePerceptron(iris_list, T, mu_tab)
+print(resultat_perceptron_Iris)
+#print("BCancer Apprentissage:\n")
+#resultat_perceptron_BCancer = executePerceptron(bCancer_list, T, mu_tab)
+#print(resultat_perceptron_BCancer)
+#print("Mushroom Apprentissage:\n")
+#resultat_perceptron_Mushroom = executePerceptron(mushroom_list, T, mu_tab)
+#print(resultat_perceptron_Mushroom)
+#print("Spambase Apprentissage:\n")
+#resultat_perceptron_Spambase = executePerceptron(spambase_list, T, mu_tab)
+#print(resultat_perceptron_Spambase)
+
+# Partie test
+iris_listTest = recuplistfich("iris_test.txt")
+#bCancer_listTest = recuplistfich("bCancer_test.txt")
+#mushroom_listTest = recuplistfich("mushroom_test.txt")
+#spambase_listTest = recuplistfich("spambase_test.txt")
+
+
+# Autres initialisations
+Niris = len(iris_listTest)
+#NbCancer = len(bCancer_listTest)
+#Nmushroom = len(mushroom_listTest)
+#Nspambase = len(spambase_listTest)
+
+perf_Iris = perfPerceptron(resultat_perceptron_Iris,iris_listTest,Niris)
+#perf_bCancer = perfPerceptron(resultat_perceptron_BCancer,bCancer_listTest,NbCancer)
+#perf_mushroom = perfPerceptron(resultat_perceptron_Mushroom,mushroom_listTest,Nmushroom)
+#perf_spambase = perfPerceptron(resultat_perceptron_Spambase,spambase_listTest,Nspambase)
+
+print ("\n Performances Perceptron Iris: \n")
+print (perf_Iris)
+#print ("\n Performances Perceptron bCancer: \n")
+#print (perf_bCancer)
+#print ("\n Performances Perceptron Mushroom: \n")
+#print (perf_mushroom)
+#print ("\n Performances Perceptron Spambase: \n")
+#print (perf_spambase)
