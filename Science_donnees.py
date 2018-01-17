ef70bc7ef1cd4aca75db4edfc1294911d11d787b
douria <douria.zen@gmail.com>
2018-01-17 11:02:05 +0100

    science_donnes.py completee et fini

--- Science_donnees.py
+++ Science_donnees.py
@@ -10,86 +10,92 @@ Created on Wed Dec 13 10:47:15 2017
 def RBCancer():
     with open("BCancer.data","r") as fichier:
         fich_donnees = fichier.read()
-    fich_res=""
+    fichRes = open("Cancer_resultat1.txt", 'w')    
+
     listdonnees = fich_donnees.split("\n")
-    
-    fich_resultat=""
+
     classe="M"
     listeDBC=normaliser(listdonnees,1)
     for i in range(len(listeDBC)-1):
        # listl=listeDBC[i].split(",")
         if(listeDBC[i][1]==classe):
-            fich_resultat+="+1"
+             fichRes.write("+1") 
         else :
-            fich_resultat+="-1"
+            fichRes.write("-1") 
         for j in range (len(listeDBC[i])-1):
            if(j>1):
-               fich_resultat+=" "+str(listeDBC[i][j])
+                fichRes.write(" "+str(listeDBC[i][j]))
         if(i!=len(listeDBC)-1):
-            fich_resultat+="\n"
-    return fich_resultat
+             fichRes.write("\n")
+    fichRes.close() 
+
 
 def spambase():
     with open("spambase.data","r") as fichier:
         fich_donnees=fichier.read()
-    fich_res=""
+
     listdonnees = fich_donnees.split("\n")
     
-    fich_resultat=""
     classe="1"
     listeM=normaliser(listdonnees,0)
+    
+    fichRes = open("spambase_resultat1.txt", 'w')    
     for i in range(len(listeM)-1):
         if(listeM[i][0]==classe):
-            fich_resultat+="+1"
+            fichRes.write("+1")  
         else :
-            fich_resultat+="-1"
+            fichRes.write("-1")  
         for j in range (len(listeM[i])-1):
            if(j>1):
-               fich_resultat+=" "+str(listeM[i][j])
+               fichRes.write(" "+str(listeM[i][j]))  
+
         if(i!=len(listeM)-1):
-            fich_resultat+="\n"
-    return fich_resultat
+             fichRes.write("\n")
+    fichRes.close() 
+
     
 def mushroom():
     with open("mushroom.data","r") as fichier:
          fich_donnees=fichier.read()
-    fich_res=""
     listdonnees = fich_donnees.split("\n")
     
-    fich_resultat=""
     listeM=normaliserAlph(listdonnees)
+    fichRes = open("mushroom_resultat1.txt", 'w')    
+
     for i in range(len(listeM)-1):
         if(listeM[i][0]=='p'):
-            fich_resultat+="+1"
+            fichRes.write("+1") 
         else :
-            fich_resultat+="-1"
+            fichRes.write("-1") 
         for j in range (len(listeM[i])-1):
            if(j>1):
-               fich_resultat+=" "+str(listeM[i][j])
+               fichRes.write(" "+str(listeM[i][j])) 
         if(i!=len(listeM)-1):
-            fich_resultat+="\n"
-    return fich_resultat
+            fichRes.write("\n")
+    fichRes.close() 
+
     
     
 def iris():
     with open("iris.data","r") as fichier:
          fich_donnees=fichier.read()
-    fich_res=""
+
     listdonnees = fich_donnees.split("\n")    
     
-    listN = normaliser(listdonnees)
-    fich_resultat=""
+    listN = normaliser(listdonnees,0)
+    fichRes = open("iris_resultat1.txt", 'w')    
+
     classe="Iris-setosa"
     for i in range(len(listdonnees)-1):
         listl=listdonnees[i].split(",")
         if(listl[4]==classe):
-            fich_resultat+="+1 "
+            fichRes.write("+1") 
         else :
-            fich_resultat+="-1 "
-        fich_resultat+=listN[i][0]+" "+listN[i][1]+" "+listN[i][2]+" "+listN[i][3]
+            fichRes.write("-1") 
+        fichRes.write(str(listN[i][0])+" "+str(listN[i][1])+" "+str(listN[i][2])+" "+str(listN[i][3])) 
         if(i!=len(listdonnees)-1):
-            fich_resultat+="\n"
-    return fich_resultat
+            fichRes.write("\n")
+    fichRes.close() 
 
 
 def normaliserAlph (tabLecture): 
@@ -133,8 +139,11 @@ def normaliser (tabLecture,startindex):
               listl[j]=float(listl[j])/float(listMax[j])
        listTab.append(listl)
    return listTab
-   
 
+iris()
+spambase()
+mushroom()
+RBCancer()
 #on affiche le resultat 
 #fich_res=spambase(listdonnees)
 #print fich_res
